a895e327f5ace531d6c4a2cc3d55c42d50f80b68
Umut <umutsahin@protonmail.com>
2017-11-23 22:50:08 +0300

    New Options Added To Select Which Tests To Run

--- test.py
+++ test.py
@@ -4,8 +4,12 @@
 # |/__\|/__\|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|
 
 
-def minority_shape_intersect(first_minority_shape, second_minority_shape):
-    return ()
+# def minority_shape_intersect(first_minority_shape, second_minority_shape):
+#     return ()
+
+
+# Or import the2.py
+import the2
 
 
 #   *   )             )
@@ -21,46 +25,47 @@ def minority_shape_intersect(first_minority_shape, second_minority_shape):
 SENSITIVITY = 0.001
 tests = []
 
+# This means run all tests. You can specify which tests to run by setting this to ["X", "Uber Basic"] etc.
+wantedTests = []
 
 # Test X (On the instruction PDF)
+if len(wantedTests) == 0 or wantedTests.find("X") < 0:
+    text_x_calculations = the2.minority_shape_intersect(
+        [(4., 8.), (20.6, 10.), (9.4, 18.1)],
+        [(12.5, 7.), (18.7, 16.2), (2., 12.), (12.5, 11.3)]
+    )
 
-text_x_calculations = minority_shape_intersect(
-    [(4., 8.), (20.6, 10.), (9.4, 18.1)],
-    [(12.5, 7.), (18.7, 16.2), (2., 12.), (12.5, 11.3)]
-)
+    test_x_results = [
+        (12.5, 11.3),
+        (12.5, 9.024096385542167),
+        (13.984606613454961, 9.202964652223491),
+        (16.513454260733393, 12.955448257862459),
+        (13.748900224891674, 14.954813230212277),
+        (6.781560380848003, 13.202548119734228),
+        (5.996175908221797, 11.733588272785214)
+    ]
 
-test_x_results = [
-    (12.5, 11.3),
-    (12.5, 9.024096385542167),
-    (13.984606613454961, 9.202964652223491),
-    (16.513454260733393, 12.955448257862459),
-    (13.748900224891674, 14.954813230212277),
-    (6.781560380848003, 13.202548119734228),
-    (5.996175908221797, 11.733588272785214)
-]
+    test_x = ("X", test_x_results, text_x_calculations)
 
-test_x = ("X", test_x_results, text_x_calculations)
-
-tests.append(test_x)
-
+    tests.append(test_x)
 
 # Test Uber Basic
+if len(wantedTests) == 0 or wantedTests.find("Uber Basic") < 0:
+    test_uber_basic_calculations = the2.minority_shape_intersect(
+        [(-6, 6), (0, 6), (0, -6), (-6, -6)],
+        [(-2, -2), (-2, 2), (0, 0)]
+    )
 
-test_uber_basic_calculations = minority_shape_intersect(
-    [(-6, 6), (0, 6), (0, -6), (-6, -6)],
-    [(-2, -2), (-2, 2), (0, 0)]
-)
+    test_uber_basic_results = [
+        (-2, -2),
+        (-2, 2),
+        (0, 0)
+    ]
 
-test_uber_basic_results = [
-    (-2, -2),
-    (-2, 2),
-    (0, 0)
-]
+    test_uber_basic = ("Uber Basic", test_uber_basic_results,
+                       test_uber_basic_calculations)
 
-test_uber_basic = ("Uber Basic", test_uber_basic_results,
-                   test_uber_basic_calculations)
-
-tests.append(test_uber_basic)
+    tests.append(test_uber_basic)
 
 
 # Insert your own cool named test here
