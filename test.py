6929ab640d5196e0c6dedcc9d6b633e81aa7c08d
Umut <umutsahin@protonmail.com>
2017-11-23 23:28:49 +0300

    Optional Testing added for Arda Karas Test

--- test.py
+++ test.py
@@ -34,6 +34,8 @@ wantedTests = []
 # Whether to show calculated input or not
 show_calculated_input = True
 
+### Example Tests ###
+
 # Test X (On the instruction PDF)
 if len(wantedTests) == 0 or wantedTests.find("X") < 0:
     text_x_calculations = the2.minority_shape_intersect(
@@ -73,99 +75,114 @@ if len(wantedTests) == 0 or wantedTests.find("Uber Basic") < 0:
 
     tests.append(test_uber_basic)
 
+### End Of Example Tests ###
+
+### Tests in honour of Arda Kara ###
+
+# Test One Point In
+if len(wantedTests) == 0 or wantedTests.find("One Point In") < 0:
+
+    test_triangle_ONEPOINTIN_calculations = the2.minority_shape_intersect(
+        [(1.1, 6.9), (2.8, 4.3), (5.3, 8.2)],
+        [(3., 7.), (5.9, 4.), (8., 7.1)]
+    )
+
+    test_triangle_ONEPOINTIN_results = [
+        (3.920414673046252, 6.047846889952153),
+        (4.550649350649351, 7.031012987012987),
+        (3.0, 7.0)
+    ]
+
+    test_triangle_ONEPOINTIN = ("One Point In",
+                                test_triangle_ONEPOINTIN_results, test_triangle_ONEPOINTIN_calculations)
+
+    tests.append(test_triangle_ONEPOINTIN)
+
+# Test Two Point In
+if len(wantedTests) == 0 or wantedTests.find("Two Point In") < 0:
+    test_triangle_TWOPOINTIN_calculations = the2.minority_shape_intersect(
+        [(0.1, 0.1), (0.5, 0.1), (0.1, 1.0)],
+        [(0.2, 0.5), (0.3, 0.3), (0.3, 1.0)]
+    )
+
+    test_triangle_TWOPOINTIN_results = [
+        (0.3, 0.5500000000000002),
+        (0.23793103448275862, 0.6896551724137931),
+        (0.2, 0.5),
+        (0.3, 0.3)
+    ]
+
+    test_triangle_TWOPOINTIN = ("Two Point In",
+                                test_triangle_TWOPOINTIN_results, test_triangle_TWOPOINTIN_calculations)
+
+    tests.append(test_triangle_TWOPOINTIN)
+
+# Test Whole Inside
+if len(wantedTests) == 0 or wantedTests.find("Whole Inside") < 0:
+    test_triangle_WHOLEINSIDE_calculations = the2.minority_shape_intersect(
+        [(6., 6.), (9., 1.), (12., 6.)],
+        [(8., 5.), (8., 4.), (9., 3.)]
+    )
+
+    test_triangle_WHOLEINSIDE_results = [(8., 5.), (8., 4.), (9., 3.)]
+
+    test_triangle_WHOLEINSIDE = ("Whole Inside",
+                                 test_triangle_WHOLEINSIDE_results, test_triangle_WHOLEINSIDE_calculations)
+
+    tests.append(test_triangle_WHOLEINSIDE)
+
+# Test Adjacent Edge
+if len(wantedTests) == 0 or wantedTests.find("Adjacent Edge") < 0:
+    test_triangle_WHOLEINSIDE_calculations = the2.minority_shape_intersect(
+        [(6., 6.), (9., 1.), (12., 6.)],
+        [(6., 6.), (12., 6.), (6., 10.)]
+    )
+
+    test_triangle_WHOLEINSIDE_results = [(6., 6.), (12., 6.)]
+
+    test_triangle_WHOLEINSIDE = ("Adjacent Edge",
+                                 test_triangle_WHOLEINSIDE_results, test_triangle_WHOLEINSIDE_calculations)
+
+    tests.append(test_triangle_WHOLEINSIDE)
+
+# Test Point on Edge
+if len(wantedTests) == 0 or wantedTests.find("Point on Edge") < 0:
+    test_triangle_POINTONEDGE_calculations = the2.minority_shape_intersect(
+        [(6., 6.), (9., 1.), (12., 6.)],
+        [(7., 6.), (8., 8.), (4., 8.)]
+    )
+
+    test_triangle_POINTONEDGE_results = [(7., 6.)]
+
+    test_triangle_POINTONEDGE = ("Point on Edge",
+                                 test_triangle_POINTONEDGE_results, test_triangle_POINTONEDGE_calculations)
+
+    tests.append(test_triangle_POINTONEDGE)
+
+# Test Common Corner
+if len(wantedTests) == 0 or wantedTests.find("Common Corner") < 0:
+    test_triangle_COMMONCORNER_calculations = the2.minority_shape_intersect(
+        [(6., 6.), (9., 1.), (12., 6.)],
+        [(6., 6.), (8., 8.), (4., 8.)]
+    )
+
+    test_triangle_COMMONCORNER_results = [(6., 6.)]
+
+    test_triangle_COMMONCORNER = ("Common Corner",
+                                  test_triangle_COMMONCORNER_results, test_triangle_COMMONCORNER_calculations)
+
+    tests.append(test_triangle_COMMONCORNER)
+
+### End Of Arda Kara ###
+
+
 # Insert your own cool named test here
 
 # Right below this comment
 
-# Tests in honour of Arda Kara
-
-# Triangle Intersection Test Set
-
-# One Point In
-test_triangle_ONEPOINTIN_calculations = the2.minority_shape_intersect(
-    [(1.1,6.9),(2.8,4.3),(5.3,8.2)],
-    [(3.,7.),(5.9,4.),(8.,7.1)]
-)
-
-test_triangle_ONEPOINTIN_results = [
-    (3.920414673046252, 6.047846889952153), 
-    (4.550649350649351, 7.031012987012987), 
-    (3.0, 7.0)
-]
-
-test_triangle_ONEPOINTIN = ("Triangle Intersection with One Point Inside", test_triangle_ONEPOINTIN_results, test_triangle_ONEPOINTIN_calculations)
-
-tests.append(test_triangle_ONEPOINTIN)
-
-# Two Point In
-test_triangle_TWOPOINTIN_calculations = the2.minority_shape_intersect(
-    [(0.1,0.1),(0.5,0.1),(0.1,1.0)],
-    [(0.2,0.5),(0.3,0.3),(0.3,1.0)]
-)
-
-test_triangle_TWOPOINTIN_results = [
-    (0.3, 0.5500000000000002), 
-    (0.23793103448275862, 0.6896551724137931), 
-    (0.2, 0.5), 
-    (0.3, 0.3)
-]
-
-test_triangle_TWOPOINTIN = ("Triangle Intersection with Two Points Inside", test_triangle_TWOPOINTIN_results, test_triangle_TWOPOINTIN_calculations)
-
-tests.append(test_triangle_TWOPOINTIN)
-
-# Whole Inside
-test_triangle_WHOLEINSIDE_calculations = the2.minority_shape_intersect(
-    [(6.,6.),(9.,1.),(12.,6.)],
-    [(8.,5.),(8.,4.),(9.,3.)]
-)
-
-test_triangle_WHOLEINSIDE_results = [(8.,5.),(8.,4.),(9.,3.)]
-
-test_triangle_WHOLEINSIDE = ("Triangle Intersesection with one in another", test_triangle_WHOLEINSIDE_results, test_triangle_WHOLEINSIDE_calculations)
-
-tests.append(test_triangle_WHOLEINSIDE)
-
-# Adjacent Edge
-test_triangle_WHOLEINSIDE_calculations = the2.minority_shape_intersect(
-    [(6.,6.),(9.,1.),(12.,6.)],
-    [(6.,6.),(12.,6.),(6.,10.)]
-)
-
-test_triangle_WHOLEINSIDE_results = [(6.,6.),(12.,6.)]
-
-test_triangle_WHOLEINSIDE = ("Triangle Intersection with an Adjacent Edge", test_triangle_WHOLEINSIDE_results, test_triangle_WHOLEINSIDE_calculations)
-
-tests.append(test_triangle_WHOLEINSIDE)
-
-# Point on Edge
-test_triangle_POINTONEDGE_calculations = the2.minority_shape_intersect(
-    [(6.,6.),(9.,1.),(12.,6.)],
-    [(7.,6.),(8.,8.),(4.,8.)]
-)
-
-test_triangle_POINTONEDGE_results = [(7.,6.)]
-
-test_triangle_POINTONEDGE = ("Triangle Intersection with One Corner on Edge of the Other", test_triangle_POINTONEDGE_results, test_triangle_POINTONEDGE_calculations)
-
-tests.append(test_triangle_POINTONEDGE)
-
-
-# Common Corner
-test_triangle_COMMONCORNER_calculations = the2.minority_shape_intersect(
-    [(6.,6.),(9.,1.),(12.,6.)],
-    [(6.,6.),(8.,8.),(4.,8.)]
-)
-
-test_triangle_COMMONCORNER_results = [(6.,6.)]
-
-test_triangle_COMMONCORNER = ("Triangle Intersection with Only One Common Corner", test_triangle_COMMONCORNER_results, test_triangle_COMMONCORNER_calculations)
-
-tests.append(test_triangle_COMMONCORNER)
-
-# End Of Arda Kara
 
 # Ok thats enough
+
 
 import datetime
 print ""
